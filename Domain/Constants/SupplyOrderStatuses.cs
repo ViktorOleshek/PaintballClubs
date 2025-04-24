cb7dde6d54102e59c0ffda4422e00a0c9901418a
Viktor <Viktor.Oleshek.PZ.2022@lpnu.ua>
2025-04-24 17:47:32 +0300

    Separate page by component

---
+++ Domain/Constants/SupplyOrderStatuses.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Constants;
+
+public static class SupplyOrderStatuses
+{
+    public const string Pending = "Pending";
+    public const string Confirmed = "Confirmed";
+    public const string Completed = "Completed";
+    public const string Cancelled = "Cancelled";
+    public const string Processing = "Processing";
+}
