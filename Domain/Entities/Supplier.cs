0b650e96871eaa486f3a03c2efdd5de021475720
Viktor <Viktor.Oleshek.PZ.2022@lpnu.ua>
2025-05-02 01:20:59 +0300

    Add suppliers page

--- Domain/Entities/Supplier.cs
+++ Domain/Entities/Supplier.cs
@@ -8,21 +8,31 @@ public class Supplier
     [Key]
     public int supplier_id { get; set; }
 
-    [StringLength(255)]
+    [Required(ErrorMessage = "Назва є обов'язковою")]
+    [StringLength(255, ErrorMessage = "Назва не може перевищувати 255 символів")]
+    [Display(Name = "Назва")]
     public string name { get; set; } = null!;
 
-    [StringLength(100)]
+    [StringLength(100, ErrorMessage = "Номер телефону не може перевищувати 100 символів")]
+    [Display(Name = "Номер телефону")]
+    [Phone(ErrorMessage = "Некоректний формат номера телефону")]
     public string? phone_number { get; set; }
 
-    [StringLength(255)]
+    [StringLength(255, ErrorMessage = "Email не може перевищувати 255 символів")]
+    [EmailAddress(ErrorMessage = "Некоректний формат електронної пошти")]
+    [RegularExpression(@"^.+@..+\..+$", ErrorMessage = "Email має бути у форматі, подібному до example@domain.com")]
+    [Display(Name = "Email")]
     public string? email { get; set; }
 
-    [StringLength(255)]
+    [StringLength(255, ErrorMessage = "Адреса не може перевищувати 255 символів")]
+    [Display(Name = "Адреса")]
     public string? address { get; set; }
 
     [Column(TypeName = "datetime")]
+    [Display(Name = "Дата створення")]
     public DateTime? CreateDatetime { get; set; }
 
     [Column(TypeName = "datetime")]
+    [Display(Name = "Дата оновлення")]
     public DateTime? UpdateDatetime { get; set; }
 }
