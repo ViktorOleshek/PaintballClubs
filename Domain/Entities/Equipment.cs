cbd07a947b688abadfe2944dc6c1548b2f80b738
Viktor <Viktor.Oleshek.PZ.2022@lpnu.ua>
2025-05-02 02:50:22 +0300

    Add equipment page

--- Domain/Entities/Equipment.cs
+++ Domain/Entities/Equipment.cs
@@ -6,25 +6,38 @@ namespace Domain.Entities;
 public class Equipment
 {
     [Key]
+    [Display(Name = "Ідентифікатор обладнання")]
     public int equipment_id { get; set; }
 
-    [StringLength(255)]
+    [Required(ErrorMessage = "Назва є обов'язковою")]
+    [StringLength(255, ErrorMessage = "Назва не може перевищувати 255 символів")]
+    [Display(Name = "Назва обладнання")]
     public string name { get; set; } = null!;
 
+    [Display(Name = "Опис")]
     public string? description { get; set; }
 
+    [Required(ErrorMessage = "Бренд є обов'язковим")]
+    [Display(Name = "Бренд")]
     public int brand_id { get; set; }
 
+    [Required(ErrorMessage = "Категорія є обов'язковою")]
+    [Display(Name = "Категорія")]
     public int category_id { get; set; }
 
+    [Required(ErrorMessage = "Тип є обов'язковим")]
+    [Display(Name = "Тип")]
     public int type_id { get; set; }
 
-    [StringLength(100)]
+    [StringLength(100, ErrorMessage = "Модель не може перевищувати 100 символів")]
+    [Display(Name = "Модель")]
     public string? model { get; set; }
 
     [Column(TypeName = "datetime")]
+    [Display(Name = "Дата створення")]
     public DateTime? CreateDatetime { get; set; }
 
     [Column(TypeName = "datetime")]
+    [Display(Name = "Дата оновлення")]
     public DateTime? UpdateDatetime { get; set; }
 }
