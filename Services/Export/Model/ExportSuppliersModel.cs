5c7699b5f25f90138d0b8d4f51c8bee1b284ec54
Viktor <Viktor.Oleshek.PZ.2022@lpnu.ua>
2025-05-08 15:19:13 +0300

    Implement export for Suppliers

--- Services/Export/Model/ExportSuppliersModel.cs
+++ Services/Export/Model/ExportSuppliersModel.cs
@@ -1,12 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
-namespace Services.Export.Model
+namespace Services.Export.Model;
+
+public class ExportSuppliersModel
 {
-    internal class ExportSuppliersModel
-    {
-    }
+    [Display(Name = "Назва постачальника")]
+    public bool IncludeName { get; set; } = true;
+
+    [Display(Name = "Телефон")]
+    public bool IncludePhone { get; set; } = true;
+
+    [Display(Name = "Email")]
+    public bool IncludeEmail { get; set; } = true;
+
+    [Display(Name = "Адреса")]
+    public bool IncludeAddress { get; set; } = true;
+
+    [Display(Name = "Загальна кількість обладнання")]
+    public bool IncludeEquipmentCount { get; set; } = true;
+
+    [Display(Name = "Дата останньої поставки")]
+    public bool IncludeLastSupplyDate { get; set; } = true;
+
+    [Display(Name = "Формат файлу")]
+    public ExportFormat FileFormat { get; set; } = ExportFormat.CSV;
+
+    [Display(Name = "Застосувати поточні фільтри")]
+    public bool ApplyCurrentFilters { get; set; } = true;
+}
+
+public enum ExportFormat
+{
+    [Display(Name = "CSV")]
+    CSV,
+
+    [Display(Name = "JSON")]
+    JSON
 }
